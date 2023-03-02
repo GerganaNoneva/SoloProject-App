fd5da5f22b0a0a9aae833759fceb01a09fd04ead
GerganaNoneva <gergananoneva@gmail.com>
2023-03-02 11:12:01 +0200

    Name Auctions

--- src/components/test/Test.jsx
+++ src/components/test/Test.jsx
@@ -9,7 +9,7 @@ import Avatar from "../avatar/Avatar"
 import User from "../user/User"
 import NftCard from "../card/Card"
 import Trending from "../trending/Trending"
-import LiveAuctions from "../liveAuctions/Auctions";
+import LiveAuctions from "../auctions/Auctions";
 
 const nftCards=[ {
    "name":"Ivy",
