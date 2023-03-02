8af6cb6ea6da83dd5c1c92baa6453f544b27e34b
GerganaNoneva <gergananoneva@gmail.com>
2023-03-02 16:40:39 +0200

    Numbers

--- src/components/how/Step.jsx
+++ src/components/how/Step.jsx
@@ -7,8 +7,8 @@ import Grid from "@mui/material/Grid";
 export default function Step ({ number, title, description }) {
     return (
         <Grid container className={classNames(styles.containerStep)}>
-            <Grid item xs={4} className={classNames(styles.number)}>
-                <div className={classNames(styles.numberDiv)}> {number} </div>
+            <Grid item xs={4} className={classNames(styles.numbers)}>
+                <div className={classNames(styles.numbersDiv)}> {number} </div>
             </Grid>
             <Grid item xs={8} className={classNames(styles.info)} >
                 <div className={classNames(styles.infoDiv)}>
